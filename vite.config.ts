b9997f5dd133d69cec8ca07dfd71088ed9c9f55d
akpi816218 <gh@akpi.is-a.dev>
2024-01-18 07:43:15 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
 					{
 						src: '/maskable-icon-512x512.png',
 						sizes: '512x512',
-						type: 'image/png'
+						type: 'image/png',
+						purpose: 'maskable'
 					},
 					{ src: 'pwa-64x64.png', sizes: '64x64', type: 'image/png' },
 					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
